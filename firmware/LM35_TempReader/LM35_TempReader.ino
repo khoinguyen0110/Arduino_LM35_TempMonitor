7d032a83a21d5889fe051d10973d8369ef37711e
Lộc <scnguyen@gmail.com>
2026-04-13 11:46:36 +0700

    feat: doi dinh dang Serial sang JSON

--- firmware/LM35_TempReader/LM35_TempReader.ino
+++ firmware/LM35_TempReader/LM35_TempReader.ino
@@ -1,32 +1,18 @@
-// ---------------------------------------------------------
-// Tên dự án: Hệ thống giám sát nhiệt độ LM35
-// Tệp tin:   LM35_TempReader.ino
-// Chức năng: Đọc nhiệt độ 2 kênh (A0, A1) và xuất định dạng CSV
-// ---------------------------------------------------------
-
-const int LM35_PIN_1 = A0; // Cảm biến 1 nối với A0
-const int LM35_PIN_2 = A1; // Cảm biến 2 nối với A1
+int adcValues[2]; float nhietDo[2]; char chuoi[40];
 
 void setup() {
-  Serial.begin(9600); 
-  // In dòng tiêu đề (Header) cho file CSV
-  Serial.println("Temp1,Temp2");
+  Serial.begin(9600);
+  delay(100);
 }
 
 void loop() {
-  // Bước 1: Đọc giá trị Analog từ 2 kênh
-  int adc1 = analogRead(LM35_PIN_1);
-  int adc2 = analogRead(LM35_PIN_2);
-
-  // Bước 2: Chuyển đổi sang độ C (sử dụng công thức đã tối ưu)
-  float temp1 = (adc1 * 500.0) / 1023.0;
-  float temp2 = (adc2 * 500.0) / 1023.0;
-
-  // Bước 3: Gửi dữ liệu theo chuẩn CSV (Giá trị 1, Giá trị 2)
-  Serial.print(temp1);
-  Serial.print(","); // Chèn dấu phẩy ngăn cách
-  Serial.println(temp2); // println ở giá trị cuối để tự động xuống dòng
-
-  // Tạm dừng 1 giây
-  delay(1000);
+  adcValues[0] = analogRead(A0);
+  nhietDo[0] = (adcValues[0] * 500.0) / 1023.0;
+  
+  adcValues[1] = analogRead(A1);
+  nhietDo[1] = (adcValues[1] * 500.0) / 1023.0;
+  
+  sprintf(chuoi, "{\"A0\":%d, \"A1\":%d}\n", (int)nhietDo[0], (int)nhietDo[1]);
+  Serial.print(chuoi);
+  delay(100);
 }
